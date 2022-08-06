5383056a5c2af0a1f9a6e1d9a747621acde420c4
Cosdaman <kjc.ang@gmail.com>
2022-08-06 10:45:12 -0700

    formatted branch data

--- loanAndCredit.py
+++ loanAndCredit.py
@@ -22,15 +22,22 @@ df_cust_raw['CUST_PHONE'] = df_cust_raw['CUST_PHONE'].map(str)
 df_cust_raw['CUST_PHONE'] = df_cust_raw['CUST_PHONE'].str[:3] + \
     '-'+df_cust_raw['CUST_PHONE'].str[3:]
 
+# arrange columns
 custCols = ['SSN', 'FIRST_NAME', 'MIDDLE_NAME', 'LAST_NAME',
             'CREDIT_CARD_NO', 'FULL_STREET_ADDRESS', 'CUST_CITY', 'CUST_COUNTRY', 'CUST_ZIP', 'CUST_PHONE', 'CUST_EMAIL', 'LAST_UPDATED']
+
 df_cust_formatted = df_cust_raw[custCols]
-print(df_cust_formatted.head())
 
-
+# BRANCH DATA
 df_branch_raw = pd.read_json('cdw_sapp_branch.json', lines=True)
 
+# FORMAT BRANCH DATA
+df_branch_raw['BRANCH_PHONE'] = df_branch_raw['BRANCH_PHONE'].map(str)
+df_branch_raw['BRANCH_PHONE'] = "("+df_branch_raw['BRANCH_PHONE'].str[:3]+")" + \
+    df_branch_raw['BRANCH_PHONE'].str[3:6] + \
+    "-"+df_branch_raw['BRANCH_PHONE'].str[6:]
 
+# CREDIT DATA
 df_credit = pd.read_json('cdw_sapp_credit.json', lines=True)
 
 
