c2aeb2acef58462ea2e0c9b2125ab9bebaf51941
Cosdaman <kjc.ang@gmail.com>
2022-08-11 16:42:29 -0700

    added more menu options

--- LoanAndCredit_ConsoleApplication.py
+++ LoanAndCredit_ConsoleApplication.py
@@ -3,41 +3,64 @@ import loadCredit
 
 
 def mainMenu():
+    print("--Main Menu--")
     print("Hello, please select the data you wish to view from the following options:")
     print('1 - Transactions')
     print('2 - Customers')
     print('3 - Load Data into Database')
     print('0 - Exit')
+    choice = input("Choice: ")
+    return choice
+
+
+def transactionsMenu():
+    print("--Transactions Menu--")
+    print("Choose to display total transactions from the following options:")
+    print("1 - Date and Zip Code")
+    print("2 - Transaction Type")
+    print("3 - State")
+    choice = input("Choice: ")
+    return choice
+
+
+def transactionsChoice():
+    return 0
 
 
 intro = "LOAN      AND     CREDIT"
 Art = text2art(intro, font='doom')
 print(Art)
 
-mainMenu()
-catChoice = input("Choice: ")
+mChoice = mainMenu()
 
-while catChoice != '0':
-    match catChoice:
+while mChoice != '0':
+    match mChoice:
         case "1":
-            print("transactions")
-            catChoice = input("Choice: ")
+            tchoice = transactionsMenu()
+            while tchoice != '0':
+                match tchoice:
+                    case '1':
+                        print('date and zipcode')
+                        tchoice = transactionsMenu()
+
+                    case _:
+                        print(
+                            "Invalid choice, please pick valid options from the menu.")
+                        tchoice = transactionsMenu()
 
         case "2":
             print("customers")
-            catChoice = input("Choice: ")
+            mChoice = mainMenu()
 
         case "3":
             loadCredit.loadData()
-            mainMenu()
-            catChoice = input("Choice: ")
+            mChoice = mainMenu()
 
         case "0":
             print("exit")
 
         case _:
             print("Invalid choice, please pick valid options from the menu.")
-            mainMenu()
-            catChoice = input("Choice: ")
+            choice = mainMenu()
 
 print("Goodbye.")
