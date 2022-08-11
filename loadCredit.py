e70bd7fdc62327b23a62f5b8698ed74e27bf559a
Cosdaman <kjc.ang@gmail.com>
2022-08-11 14:52:29 -0700

    finalized database import

--- loadCredit.py
+++ loadCredit.py
@@ -3,6 +3,8 @@ import pandas as pd
 from pyspark.sql import SparkSession
 from dotenv import dotenv_values
 
+print('Beginning data import...')
+
 
 config = dotenv_values(".env")
 dbuser = config["dbuser"]
@@ -36,9 +38,11 @@ df_cust_raw['CUST_PHONE'] = df_cust_raw['CUST_PHONE'].map(str)
 df_cust_raw['CUST_PHONE'] = df_cust_raw['CUST_PHONE'].str[:3] + \
     '-'+df_cust_raw['CUST_PHONE'].str[3:]
 
+df_cust_raw['LAST_UPDATED'] = pd.to_datetime(df_cust_raw['LAST_UPDATED'])
+
 # arrange columns
 custCols = ['SSN', 'FIRST_NAME', 'MIDDLE_NAME', 'LAST_NAME',
-            'CREDIT_CARD_NO', 'FULL_STREET_ADDRESS', 'CUST_CITY', 'CUST_COUNTRY', 'CUST_ZIP', 'CUST_PHONE', 'CUST_EMAIL', 'LAST_UPDATED']
+            'CREDIT_CARD_NO', 'FULL_STREET_ADDRESS', 'CUST_CITY', 'CUST_STATE', 'CUST_COUNTRY', 'CUST_ZIP', 'CUST_PHONE', 'CUST_EMAIL', 'LAST_UPDATED']
 
 df_cust_formatted = df_cust_raw[custCols]
 
@@ -50,6 +54,8 @@ df_branch_raw['BRANCH_PHONE'] = df_branch_raw['BRANCH_PHONE'].map(str)
 df_branch_raw['BRANCH_PHONE'] = "("+df_branch_raw['BRANCH_PHONE'].str[:3]+")" + \
     df_branch_raw['BRANCH_PHONE'].str[3:6] + \
     "-"+df_branch_raw['BRANCH_PHONE'].str[6:]
+
+df_branch_raw['LAST_UPDATED'] = pd.to_datetime(df_branch_raw['LAST_UPDATED'])
 
 df_branch_formatted = df_branch_raw.copy(deep=True)
 
@@ -67,8 +73,10 @@ df_credit_raw['DAY'] = df_credit_raw['DAY'].str.zfill(2)
 df_credit_raw['TIMEID'] = df_credit_raw['YEAR'].map(
     str) + df_credit_raw['MONTH'].map(str) + df_credit_raw['DAY'].map(str)
 
+df_credit_raw.rename(columns={"CREDIT_CARD_NO": "CUST_CC_NO"},inplace=True)
+
 # arrange columns
-creditCols = ['CREDIT_CARD_NO', 'TIMEID', 'CUST_SSN', 'BRANCH_CODE',
+creditCols = ['CUST_CC_NO', 'TIMEID', 'CUST_SSN', 'BRANCH_CODE',
               'TRANSACTION_TYPE', 'TRANSACTION_VALUE', 'TRANSACTION_ID']
 
 
@@ -92,22 +100,61 @@ sparkDF_Cust.write.format("jdbc") \
     .mode("overwrite") \
     .option("url", "jdbc:mysql://localhost:3306/creditcard_capstone") \
     .option("dbtable", "creditcard_capstone.CDW_SAPP_CUSTOMER") \
-    .option("user", "root") \
-    .option("password", "root") \
+    .option("user", dbuser) \
+    .option("password", dbpass) \
     .save()
 
 sparkDF_Credit.write.format("jdbc") \
     .mode("overwrite") \
     .option("url", "jdbc:mysql://localhost:3306/creditcard_capstone") \
     .option("dbtable", "creditcard_capstone.CDW_SAPP_CREDIT_CARD") \
-    .option("user", "root") \
-    .option("password", "root") \
+    .option("user", dbuser) \
+    .option("password", dbpass) \
     .save()
 
 sparkDF_Branch.write.format("jdbc") \
     .mode("overwrite") \
     .option("url", "jdbc:mysql://localhost:3306/creditcard_capstone") \
     .option("dbtable", "creditcard_capstone.CDW_SAPP_BRANCH") \
-    .option("user", "root") \
-    .option("password", "root") \
+    .option("user", dbuser) \
+    .option("password", dbpass) \
     .save()
+
+print("Converting SQL data types...")
+
+useDB = "USE creditcard_capstone;"
+db_cursor.execute(useDB)
+
+alterTables = [
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN SSN INT;",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN FIRST_NAME VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN MIDDLE_NAME VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN LAST_NAME VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN CREDIT_CARD_NO VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN FULL_STREET_ADDRESS VARCHAR(100);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN CUST_CITY VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN CUST_STATE VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN CUST_COUNTRY VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN CUST_ZIP INT;",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN CUST_PHONE VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CUSTOMER MODIFY COLUMN CUST_EMAIL VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_BRANCH MODIFY COLUMN BRANCH_CODE INT;",
+    "ALTER TABLE CDW_SAPP_BRANCH MODIFY COLUMN BRANCH_NAME VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_BRANCH MODIFY COLUMN BRANCH_STREET VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_BRANCH MODIFY COLUMN BRANCH_CITY VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_BRANCH MODIFY COLUMN BRANCH_STATE VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_BRANCH MODIFY COLUMN BRANCH_ZIP INT;",
+    "ALTER TABLE CDW_SAPP_BRANCH MODIFY COLUMN BRANCH_PHONE VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CREDIT_CARD MODIFY COLUMN CUST_CC_NO VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CREDIT_CARD MODIFY COLUMN TIMEID VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CREDIT_CARD MODIFY COLUMN CUST_SSN INT;",
+    "ALTER TABLE CDW_SAPP_CREDIT_CARD MODIFY COLUMN BRANCH_CODE INT;",
+    "ALTER TABLE CDW_SAPP_CREDIT_CARD MODIFY COLUMN TRANSACTION_TYPE VARCHAR(50);",
+    "ALTER TABLE CDW_SAPP_CREDIT_CARD MODIFY COLUMN TRANSACTION_VALUE DOUBLE;",
+    "ALTER TABLE CDW_SAPP_CREDIT_CARD MODIFY COLUMN TRANSACTION_ID INT;",
+]
+
+for i in alterTables:
+    db_cursor.execute(i)
+
+print("Data import complete.")
