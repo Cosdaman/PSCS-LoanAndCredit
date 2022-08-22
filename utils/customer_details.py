c7f0a40ab37a493f0170bda5515be18153ecc2e2
Cosdaman <kjc.ang@gmail.com>
2022-08-21 17:18:51 -0700

    added skeleton for db parsing

---
+++ utils/customer_details.py
@@ -0,0 +1,14 @@
+def checkCustDetails():
+    return 0
+
+
+def modifyCustDetails():
+    return 0
+
+
+def monthlyBill():
+    return 0
+
+
+def custTransactionsTwoDates():
+    return 0
---
+++ utils/transaction_details.py
@@ -0,0 +1,10 @@
+def zipcodeTransactions():
+    return 0
+
+
+def typeTransactions():
+    return 0
+
+
+def stateTransactions():
+    return 0
